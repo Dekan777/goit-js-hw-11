a693fa1b932c24adc5ba30c39edde4453ce456a6
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-28 00:46:34 +0200

    apiService

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import ImagesApiService from './js/images-service';
-// import LoadMoreBtn from './js/load-more';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// main.js
+
+import fetchData from './js/apiService';
+
+// Добавление обработчика события для формы
+document
+  .getElementById('search-form')
+  .addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const searchQuery = document.getElementById('search-query').value;
+    await fetchData(searchQuery);
+  });
