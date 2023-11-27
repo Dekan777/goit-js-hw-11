56e8811a2f3d1f46d90cc94f9a27bc55ab496096
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-27 22:11:06 +0200

    key

--- src/images_api.js
+++ src/images_api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_MCbmjbpL2EK0M1ExHHJ0clpK8ztrjpAtYLeQ5PmMGUjuQ3wzfscrLeVh8obG0Lbz';
+
+  const API_KEY = '40913963-8e579c79c98d472778d15f639';
+  const BASE_URL = 'https://pixabay.com/api/';
+
   
