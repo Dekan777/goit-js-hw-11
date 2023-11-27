d26269a93345116599a669b79d15c82dc734d2b8
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-27 20:43:19 +0200

    install

---
+++ src/images_api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+axios.defaults.headers.common['x-api-key'] =
+  'live_MCbmjbpL2EK0M1ExHHJ0clpK8ztrjpAtYLeQ5PmMGUjuQ3wzfscrLeVh8obG0Lbz';
+  
